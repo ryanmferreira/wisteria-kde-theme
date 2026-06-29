c5c0c2bea564884b185d92dc351654da0c95f37d
ryanmferreira <dev.ryanmferreira@outlook.com>
2026-06-29 12:38:57 -0300

    Update assets

--- create-symbolic-links.sh
+++ create-symbolic-links.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if ln -sfn "$PWD/aurorae/WisteriaKDE" "$HOME/.local/share/aurorae/themes/WisteriaKDE"; then
+if ln -sfn "$PWD/aurorae/wisteriakde" "$HOME/.local/share/aurorae/themes/wisteriakde"; then
     echo "Aurorae symbolic link created/updated successfully!"
 else
     echo "Failed to create Aurorae symbolic link."
 fi
 
-if ln -sfn "$PWD/plasma-style/WisteriaKDE" "$HOME/.local/share/plasma/desktoptheme/WisteriaKDE"; then
+if ln -sfn "$PWD/plasma-style/wisteriakde" "$HOME/.local/share/plasma/desktoptheme/wisteriakde"; then
     echo "Plasma Style symbolic link created/updated successfully!"
 else
     echo "Failed to create Plasma Style symbolic link."
