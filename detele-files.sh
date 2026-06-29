507aca15d09d0a01fbcc3446d562c92d8e7cd3ac
ryanmferreira <dev.ryanmferreira@outlook.com>
2026-06-29 17:38:58 -0300

    Add script to delete Wisteria KDE theme files and create symbolic links

---
+++ detele-files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if rm -rf "$HOME/.local/share/aurorae/themes/wisteriakde"; then
+    echo "Aurorae folders or symbolic link deleted successfully!"
+else
+    echo "Failed to delete Aurorae folders or symbolic link."
+fi
+
+if rm -rf "$HOME/.local/share/plasma/desktoptheme/wisteriakde"; then
+    echo "Plasma Style folders or symbolic link deleted successfully!"
+else
+    echo "Failed to delete Plasma Style folders or symbolic link."
+fi
+
+echo
+read -rp "Create symbolic links now? (y/N): " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+    bash create-symbolic-links.sh
+else
+    echo "Skipping symbolic link creation."
+fi
